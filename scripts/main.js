6204708f990926de7c126c1d8bd72bc8b15ccc48
Yu-An Lo <Yu-An Lo>
2016-06-07 11:04:57 -0700

    basic round-trip function is done.

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,6 @@ var requestContent = document.getElementById("optionsContent");
 var divContent = document.getElementById("content");
 var tripMode = document.getElementById("trips");
 var optionCollapse = true;
-var finalResultLeaveTrip = [];
 createSelections("adultNum", 10, 1);
 createSelections("childNum", 10);
 createSelections("stopNum", 4, 0);
@@ -66,16 +65,22 @@ function setLocaleTime(timeStr) {
 }
 
 //======================================================================================
-function createLabel(className, formObj, insertedTxt) {
-    var tr = tableObj.insertRow(-1);
-    tr.setAttribute("class", className);
-    var trTxt = tr.insertCell(0);
-    trTxt.innerHTML = insertedTxt;
+function buildResultBlock(Element, attribute, attributeVal, parentNode, infoTxt) {
+    var myElement = document.createElement(Element);
+    myElement.setAttribute(attribute, attributeVal);
+    var myDivResults = document.createElement("div");
+    myDivResults.setAttribute("class", "divResults");
+    if (infoTxt !== undefined) {
+        var txtNode = document.createTextNode(infoTxt);
+        myElement.appendChild(txtNode);
+    }    
+    myDivResults.appendChild(myElement);
+    parentNode.appendChild(myDivResults);
 }
 
 //======================================================================================
 tripMode.addEventListener("click", function() {
-    var returnDateLabel = document.getElementById("returnDateLabel");
+    var returnDateLabel = document.getElementById("returnDateShown");
     var returnDate = document.getElementById("returnDate");
     
     if (document.getElementById("oneWay").checked) {
@@ -85,6 +90,11 @@ tripMode.addEventListener("click", function() {
         returnDateLabel.setAttribute("style", "display: block");
         returnDate.setAttribute("min", document.getElementById("leaveDate").value);  
         console.log(document.getElementById("leaveDate").value);
+        document.getElementById("returnDateDiv").style.backgroundColor = "yellow";
+        window.setTimeout(function() {
+            document.getElementById("returnDateDiv").style.backgroundColor = "azure";    
+        }, 300);
+        
     }
 })
 
@@ -110,20 +120,18 @@ sendButton.addEventListener("click", function() {
     var solutionsVal = document.getElementById("solution").value,
         maxPriceVal = document.getElementById("price").value,
         //saleCountryVal = document.getElementById("country").value,
-        stopValUser = document.getElementById("stopNum").value;
+        stopValUser = document.getElementById("stopNum").value,
+        leaveTripNode = document.getElementById("resultLeaveTrip"),
+        returnTripNode = document.getElementById("resultReturnTrip");
     
-    if(document.getElementById("resultLeaveTrip") !== null) {
-        var parent = document.getElementById("main");
-        var child = document.getElementById("resultLeaveTrip");
-        parent.removeChild(child);
-    } 
+    while (leaveTripNode.firstChild) {
+        leaveTripNode.removeChild(leaveTripNode.firstChild);
+    }
     
-    if(document.getElementById("resultReturnTrip") !== null) {
-        var parent = document.getElementById("main");
-        var child = document.getElementById("resultReturnTrip");
-        parent.removeChild(child);
-    } 
-    
+    while (returnTripNode.firstChild) {
+        returnTripNode.removeChild(returnTripNode.firstChild);
+    }
+
     jsonRequestLeaveTrip = {
         request: {
              passengers: {
@@ -144,17 +152,56 @@ sendButton.addEventListener("click", function() {
         }
     }
     
+    
+    
+    if (document.getElementById("roundTrip").checked) {
+        jsonRequestReturnTrip = {
+            request: {
+                passengers: {
+                    adultCount: parseInt(document.getElementById("select_adultNum").value),
+                    //infantInLapCount: parseInt(document.getElementById("select_infantLapNum").value),
+                    //infantInSeatCount: parseInt(document.getElementById("select_infantSeatNum").value),
+                    childCount: parseInt(document.getElementById("select_childNum").value),
+                    //seniorCount: parseInt(document.getElementById("select_seniorNum").value)
+                },
+            slice: [
+                {
+                    origin: document.getElementById("destination").value.toUpperCase(),
+                    destination: document.getElementById("origin").value.toUpperCase(),
+                    date: document.getElementById("returnDate").value                
+                }
+            ],
+            refundable: document.getElementById("refund").checked
+            }
+        }
+    }
+    
+    //if(document.getElementById)
+    
 
-    if (maxPriceVal != "") {
+    if (maxPriceVal != "" && document.getElementById("oneWay").checked) {
         jsonRequestLeaveTrip.request["maxPrice"] = "USD" + maxPriceVal;
+    }
+    else if (maxPriceVal != "" && document.getElementById("roundTrip").checked) {
+        jsonRequestLeaveTrip.request["maxPrice"] = "USD" + maxPriceVal;
+        jsonRequestReturnTrip.request["maxPrice"] = "USD" + maxPriceVal;
     };
-/*    if (saleCountryVal != "") {
-        jsonRequest.request["saleCountry"] = saleCountryVal.toUpperCase();
-    }; */
-    if (solutionsVal != "") {
+
+    if (solutionsVal != "" && document.getElementById("oneWay").checked) {
         jsonRequestLeaveTrip.request["solutions"] = parseInt(solutionsVal);
+        getData(jsonRequestLeaveTrip, "resultLeaveTrip", "leave");
         
-        getData(jsonRequestLeaveTrip);
+        console.log(jsonRequestLeaveTrip);
+    }
+    else if (solutionsVal != "" && document.getElementById("roundTrip").checked) {
+        jsonRequestLeaveTrip.request["solutions"] = parseInt(solutionsVal);
+        jsonRequestReturnTrip.request["solutions"] = parseInt(solutionsVal);
+        
+        console.log(jsonRequestLeaveTrip);
+        console.log(jsonRequestReturnTrip);
+        
+        getData(jsonRequestLeaveTrip, "resultLeaveTrip", "Leave");
+        getData(jsonRequestReturnTrip, "resultReturnTrip", "Return");
     }
     else {
         alert("Please tell me the max number of results you want to know");
@@ -188,9 +235,10 @@ sendButton.addEventListener("click", function() {
 
 //======================================================================================
 
-function getData(jsonRequestObj) {
-    
+function getData(jsonRequestObj, targetDivID, tripDirection) {
+    var finalResult = [];
     console.log(jsonRequestObj);
+    
     
     var holder = JSON.stringify(jsonRequestObj);
     var xhr = new XMLHttpRequest();
@@ -222,7 +270,6 @@ function getData(jsonRequestObj) {
                     
                     for (var i = 0; i < numResults; i++) {
                         stops = results.trips.tripOption[i].slice[0].segment.length - 1;
-                        console.log(document.getElementById("select_stopNum").value);
                         if (stops <= document.getElementById("select_stopNum").value) {
                             finalResult.push(results.trips.tripOption[i]);
                         }                        
@@ -230,35 +277,67 @@ function getData(jsonRequestObj) {
                     
                     console.log(finalResult);
                     
-                    var resultBr = document.createElement("br");
-                    var resultForm = document.createElement("form");
-                    var resultSelectButton = document.createElement("button");
-                    
-                    var resultTable = document.createElement("table");
-                    var resultTableHeader = document.createElement("th");
-                    
-                    
-                    for (var i = 0; i < finalResult.length; i++) {
-                        stops = finalResult[i].slice[0].segment.length;
-                        var resultTr = document.getElementById("resultTable");
-                        createTr("options", resultTr, "Option#" + (i + 1));
-                        createTr("price", resultTr, "Total Price: " + finalResult[i].saleTotal);
-                        createTr("flightDuration", resultTr, "Flight Duration: " + finalResult[i].slice[0].duration + " mins");
-                        for (var j = 0; j < stops; j++) {
-                            createTr("iteration", resultTr, finalResult[i].slice[0].segment[j].flight.carrier + finalResult[i].slice[0].segment[j].flight.number + " from: " + finalResult[i].slice[0].segment[j].leg[0].origin + "-->" + "to: " + finalResult[i].slice[0].segment[j].leg[0].destination);
-                            createTr("arrivalTime", resultTr, "Arrival Time: " + setLocaleTime(finalResult[i].slice[0].segment[j].leg[0].arrivalTime));
-                            createTr("departureTime", resultTr, "Departure Time: " + setLocaleTime(finalResult[i].slice[0].segment[j].leg[0].departureTime));
-                        }
-                        
+                    if (finalResult.length < 1 && document.getElementById("resultLeaveTrip").firstChild == null) {
+                        buildResultBlock("h3", "id", tripDirection + "h3", document.getElementById("resultLeaveTrip"), "Sorry... No result was found. Please add more stops.");
                     }
-                    
+                    else {
+                        for (var i = 0; i < finalResult.length; i++) {
+                            stops = finalResult[i].slice[0].segment.length - 1;
+
+                            var myForm = document.createElement("form");
+                            myForm.setAttribute("id", tripDirection + "Trip" + (i + 1));
+                            var myFieldSet = document.createElement("fieldset");
+                            myFieldSet.setAttribute("id", tripDirection + "Fieldset" + (i + 1));
+                            var myLegend = document.createElement("legend");
+                            var legendTxt = document.createTextNode(tripDirection + "Option" + (i + 1));
+                            myLegend.appendChild(legendTxt);
+                            myFieldSet.appendChild(myLegend);
+                            myForm.appendChild(myFieldSet);
+                            document.getElementById(targetDivID).appendChild(myForm);
+
+                            buildResultBlock("button", "id", tripDirection + "button" + (i + 1), myFieldSet, "Select");
+
+
+                            //var myDiv = document.createElement("div");
+                            //myDiv.setAttribute("class", "divButton");
+                            //var myButton = document.createElement("button");
+                            //myButton.setAttribute("id", "button" + (i + 1));
+                            //var buttonTxt = document.createTextNode("Select" + (i + 1));
+                            //myButton.appendChild(buttonTxt);
+                            //myDiv.appendChild(myButton);
+                            //myFieldSet.appendChild(myDiv);
+
+                            buildResultBlock("label", "class", "priceOption" + (i + 1), myFieldSet, "Price: " + finalResult[i].saleTotal);
+                            buildResultBlock("label", "class", "durationOption" + (i + 1), myFieldSet, "Flight Duration: " + finalResult[i].slice[0].duration + " mins");
+                            for (var j = 0; j <= stops; j++) {
+                                buildResultBlock("ul", "id", tripDirection + "Stop" + (i + 1) + (j + 1), myFieldSet);
+                                buildResultBlock("li", "id", tripDirection + "ListOfStop" + (i + 1) + (j + 1), document.getElementById(tripDirection + "Stop" + (i + 1) + (j + 1)), finalResult[i].slice[0].segment[j].flight.carrier + finalResult[i].slice[0].segment[j].flight.number + " from: " + finalResult[i].slice[0].segment[j].leg[0].origin + "-->" + "to: " + finalResult[i].slice[0].segment[j].leg[0].destination);
+                                buildResultBlock("p", "class", "departingTime", document.getElementById(tripDirection + "ListOfStop" + (i + 1) + (j + 1)), "Departure Time: " + setLocaleTime(finalResult[i].slice[0].segment[j].leg[0].departureTime));
+                                buildResultBlock("p", "class", "arrivalTime", document.getElementById(tripDirection + "ListOfStop" + (i + 1) + (j + 1)), "Arrival Time: " + setLocaleTime(finalResult[i].slice[0].segment[j].leg[0].arrivalTime));
+
+                            }
+
+
+                            /*
+
+                            createTr("options", resultTr, "Option#" + (i + 1));
+                            createTr("price", resultTr, "Total Price: " + finalResult[i].saleTotal);
+                            createTr("flightDuration", resultTr, "Flight Duration: " + finalResult[i].slice[0].duration + " mins");
+                            for (var j = 0; j < stops; j++) {
+                                createTr("iteration", resultTr, finalResult[i].slice[0].segment[j].flight.carrier + finalResult[i].slice[0].segment[j].flight.number + " from: " + finalResult[i].slice[0].segment[j].leg[0].origin + "-->" + "to: " + finalResult[i].slice[0].segment[j].leg[0].destination);
+                                createTr("arrivalTime", resultTr, "Arrival Time: " + setLocaleTime(finalResult[i].slice[0].segment[j].leg[0].arrivalTime));
+                                createTr("departureTime", resultTr, "Departure Time: " + setLocaleTime(finalResult[i].slice[0].segment[j].leg[0].departureTime));
+                            }
+                            */
+                        }
+                    }
                 }
                 else {
                     var resultTable = document.createElement("table");
                     var tableNode = document.createTextNode("No Result Was Found");
                     resultTable.appendChild(tableNode);
                     resultTable.setAttribute("id", "resultTable");
-                    document.getElementById("resultContent").appendChild(resultTable);    
+                    document.getElementById(targetDivID).appendChild(resultTable);    
                 }                
             }
             else {
@@ -266,10 +345,11 @@ function getData(jsonRequestObj) {
                 var tableNode = document.createTextNode("No Result Was Found");
                 resultTable.appendChild(tableNode);
                 resultTable.setAttribute("id", "resultTable");
-                document.getElementById("resultContent").appendChild(resultTable);
+                document.getElementById("resultLeaveTrip").appendChild(resultTable);
                 var resultTr = document.getElementById("resultTable").insertRow(i);
                 var trText = resultTr.insertCell(0);
                 trText.innerHTML = "Ooops!! Something went wrong..." + "</br>" + "Please Double Check Your Input Fields" + "</br>" + "HTTP status code: " + xhr.status;
+                document.getElementById(targetDivID).appendChild(resultTable);
             }
         }
         
